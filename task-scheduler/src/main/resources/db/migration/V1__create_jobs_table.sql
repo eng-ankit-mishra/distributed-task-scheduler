fe5e3f7a3de8ed0934511cd4e29d198550ba2027
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-06-14 20:04:03 +0530

    Added flyway tables

--- task-scheduler/src/main/resources/db/migration/V1__create_jobs_table.sql
+++ task-scheduler/src/main/resources/db/migration/V1__create_jobs_table.sql
@@ -1,18 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TYPE job_status AS ENUM (
-    'PENDING',
-    'RUNNING',
-    'SUCCESS',
-    'FAILED',
-    'DEAD'
-);
-
 CREATE TABLE jobs (
                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                       name VARCHAR(255) NOT NULL,
                       cron_expression VARCHAR(100) NOT NULL,
-                      status job_status NOT NULL DEFAULT 'PENDING',
+
+    -- changed from job_status enum to VARCHAR
+                      status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+
                       payload TEXT,
                       retry_count INTEGER NOT NULL DEFAULT 0,
                       next_run_at TIMESTAMP NOT NULL,
