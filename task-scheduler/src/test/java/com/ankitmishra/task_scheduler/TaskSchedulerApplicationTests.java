e50a8faa6cdd5f043fa5d82e92da586028b18f99
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-06-15 17:35:32 +0530

    Added Spring Profile Annotation

--- task-scheduler/src/test/java/com/ankitmishra/task_scheduler/TaskSchedulerApplicationTests.java
+++ task-scheduler/src/test/java/com/ankitmishra/task_scheduler/TaskSchedulerApplicationTests.java
@@ -1,11 +1,15 @@
 package com.ankitmishra.task_scheduler;
 
+import com.ankitmishra.task_scheduler.config.TestRedisConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestRedisConfig.class)
 class TaskSchedulerApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
