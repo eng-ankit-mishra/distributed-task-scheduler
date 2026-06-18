57862ac3f58b4a3aa6108ed0e9a43b29d43a316c
Ankit Mishra <developer.ankitmishra@gmail.com>
2026-06-18 11:56:51 +0530

    Updated Redisson Config

--- task-scheduler/src/main/java/com/ankitmishra/task_scheduler/config/RedissonConfig.java
+++ task-scheduler/src/main/java/com/ankitmishra/task_scheduler/config/RedissonConfig.java
@@ -3,21 +3,31 @@ package com.ankitmishra.task_scheduler.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.IOException;
-
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
     @Bean(destroyMethod = "shutdown")
     @ConditionalOnMissingBean
-    public RedissonClient redissonClient() throws IOException {
-        Config config=Config.fromYAML(
-                getClass().getClassLoader().getResourceAsStream("redisson.yaml")
-        );
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort)
+                .setConnectionMinimumIdleSize(2)
+                .setConnectionPoolSize(10)
+                .setIdleConnectionTimeout(10000)
+                .setConnectTimeout(10000)
+                .setTimeout(3000);
         return Redisson.create(config);
     }
 }
